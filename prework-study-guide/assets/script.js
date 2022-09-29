7aa8e5ad1f468cbcb26ac013bda0bb3c63919b0d
Mattdeon08 <matthewdeon08@icloud.comgit@github.com:Mattdeon08/prework-study-guide.git git config --global user.email matthewdeon08@icloud.com>
2022-09-28 19:05:40 -0500

    <add code to java script>

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,19 +1,25 @@
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
+var topics = ['HTML', 'CSS', 'GIT', 'JavaScript'];
+var randomtopics = topics[Math.random() * topics.lenfth];
+function listTopic() {{}
 for (var x = 0; x < topics.length; x++) {
  console.log(topics[x]);
 }
+}
 
-/*
-if (topic === 'HTML') {
+function selectTopic() {
+if (topics === 'HTML') {
  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomtopics === 'CSS') {
  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomtopics === 'Git') {
  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomtopics === 'JavaScript') {
  console.log("Let's study JavaScript!");
 } else {
  console.log('Please try again!');
 }
-*/
+}
+console.log('Here are the topics we learned through Prework:');
+listTopic()
+console.log('Which topics we study first?');
+selectTopic()
